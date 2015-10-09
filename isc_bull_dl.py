c67b56f06a068e52db708f0dca4a8741d1ca71f8
Nima Nooshiri <nima.nooshiri@gfz-potsdam.de>
2015-10-09 17:53:38 +0200

    changed the style of the code according to PEP8

--- isc_bull_dl.py
+++ isc_bull_dl.py
@@ -8,13 +8,16 @@ import argparse
 import urllib
 from itertools import chain
 
+
 class Parser(argparse.ArgumentParser):
 
     # class attributes
-    __search2options = {'GLOBAL':None,
-                        'RECT':('bot_lat', 'top_lat', 'left_lon', 'right_lon'),
-                        'CIRC':('ctr_lat', 'ctr_lon', 'max_dist_units', 'radius'),
-                        'POLY':('coordvals',)}
+    __search2options = {'GLOBAL': None,
+                        'RECT': ('bot_lat', 'top_lat', 'left_lon',
+                                 'right_lon'),
+                        'CIRC': ('ctr_lat', 'ctr_lon', 'max_dist_units',
+                                 'radius'),
+                        'POLY': ('coordvals',)}
 
     __query_client = "http://www.isc.ac.uk/cgi-bin/web-db-v4?"
 
@@ -27,16 +30,17 @@ class Parser(argparse.ArgumentParser):
         conflicts = filter(None, Parser.__search2options.values())
         conflicts = list(chain(*conflicts))
 
-        if (not all([getattr(self.args,r) for r in required]) or
-            any([hasattr(self.args,c) for c in conflicts])):
+        if (not all([getattr(self.args, r) for r in required]) or
+                any([hasattr(self.args, c) for c in conflicts])):
             msg = '''Missing argument(s) for defined search shape. Check the
             dependent parameters for your considered search type.'''
             self.error(msg)
 
     def download_bulletin(self):
         args_dic = vars(self.args)
-        args_dic.update(prime_only='on', include_phases='on', include_headers='on')
-        query_opts = ["=".join((k,str(v))) for (k,v) in args_dic.items()]
+        args_dic.update(prime_only='on', include_phases='on',
+                        include_headers='on')
+        query_opts = ["=".join((k, str(v))) for (k, v) in args_dic.items()]
         query_opts = "&".join(query_opts)
 
         query_url = Parser.__query_client + query_opts
@@ -45,93 +49,129 @@ class Parser(argparse.ArgumentParser):
         return self.bulletin
 
 
-
 if __name__ == "__main__":
     parser = Parser(description='Download ISC bulletin dataset.')
-    ### Geographic region ###
+
+    # --- Geographic region ---
     parser.add_argument('--search', dest='searchshape', required=True,
-                        choices=['GLOBAL','RECT','CIRC','POLY'])
+                        choices=['GLOBAL', 'RECT', 'CIRC', 'POLY'])
 
-    ### RECT (rectangular) search type ###
+    # --- RECT (rectangular) search type ---
     rect_group = parser.add_argument_group(title='RECT-rectangular search',
-                                           description='Dependent parameters for --search=RECT')
-    rect_group.add_argument('--blat', dest='bot_lat', default=argparse.SUPPRESS, type=float,
-                            help='Bottom latitude of rectangular region (-90 to 90).')
-    rect_group.add_argument('--tlat', dest='top_lat', default=argparse.SUPPRESS, type=float,
-                            help='Top latitude of rectangular region (-90 to 90).')
-    rect_group.add_argument('--llon', dest='left_lon', default=argparse.SUPPRESS, type=float,
-                            help='Left longitude of rectangular region (-180 to 180).')
-    rect_group.add_argument('--rlon', dest='right_lon', default=argparse.SUPPRESS, type=float,
-                            help='Right longitude of rectangular region (-180 to 180).')
+                                           description='''Dependent parameters
+                                           for --search=RECT option.''')
 
-    ### CIRC (circular) search type ###
+    rect_group.add_argument('--blat', dest='bot_lat', type=float,
+                            default=argparse.SUPPRESS, help='''Bottom latitude
+                            of rectangular region (-90 to 90).''')
+
+    rect_group.add_argument('--tlat', dest='top_lat', type=float,
+                            default=argparse.SUPPRESS, help='''Top latitude of
+                            rectangular region (-90 to 90).''')
+
+    rect_group.add_argument('--llon', dest='left_lon', type=float,
+                            default=argparse.SUPPRESS, help='''Left longitude
+                            of rectangular region (-180 to 180).''')
+
+    rect_group.add_argument('--rlon', dest='right_lon', type=float,
+                            default=argparse.SUPPRESS, help='''Right longitude
+                            of rectangular region (-180 to 180).''')
+
+    # --- CIRC (circular) search type ---
     circ_group = parser.add_argument_group(title='CIRC-circular search',
-                                           description='Dependent parameters for --search=CIRC')
-    circ_group.add_argument('--clat', dest='ctr_lat', default=argparse.SUPPRESS, type=float,
-                            help='Central latitude of circular region.')
-    circ_group.add_argument('--clon', dest='ctr_lon', default=argparse.SUPPRESS, type=float,
-                            help='Central longitude of circular regio.')
-    circ_group.add_argument('--units', dest='max_dist_units', choices=['deg', 'km'], default=argparse.SUPPRESS,
+                                           description='''Dependent parameters
+                                           for --search=CIRC option.''')
+
+    circ_group.add_argument('--clat', dest='ctr_lat', type=float,
+                            default=argparse.SUPPRESS, help='''Central latitude
+                            of circular region.''')
+
+    circ_group.add_argument('--clon', dest='ctr_lon', type=float,
+                            default=argparse.SUPPRESS, help='''Central longitude
+                            of circular regio.''')
+
+    circ_group.add_argument('--units', dest='max_dist_units',
+                            choices=['deg', 'km'], default=argparse.SUPPRESS,
                             help='Units of distance for a circular search.')
-    circ_group.add_argument('--radius', dest='radius', default=argparse.SUPPRESS, type=float,
-                            help='''Radius for circular search region: 0 to 180 if
-                            --units=deg, 0 to 20015 if --units=km.''')
 
-    ### POLY (customised polygon) search type ###
+    circ_group.add_argument('--radius', dest='radius', type=float,
+                            default=argparse.SUPPRESS, help='''Radius for
+                            circular search region: 0 to 180 if --units=deg,
+                            0 to 20015 if --units=km.''')
+
+    # --- POLY (customised polygon) search type ---
     poly_group = parser.add_argument_group(title='POLY-polygon search',
-                                           description='Dependent parameters for --search=POLY')
-    poly_group.add_argument('--coords', dest='coordvals', default=argparse.SUPPRESS,
-                            help='''Comma seperated list of coordinates for a
-    desired polygon. Coordinates in the western and southern hemispheres should be
-    negative (lat1,lon1,lat2,lon2,...,latN,lonN,lat1,lon1).''')
+                                           description='''Dependent parameters
+                                           for --search=POLY''')
 
-    ### Time range ###
+    poly_group.add_argument('--coords', dest='coordvals',
+                            default=argparse.SUPPRESS, help='''Comma seperated
+                            list of coordinates for a desired polygon (lat1,
+                            lon1, lat2, lon2, ..., latN, lonN, lat1, lon1).
+                            Coordinates in the western and southern hemispheres
+                            should be negative.''')
+
+    # --- Time range ---
     parser.add_argument('--syear', dest='start_year', type=int, required=True,
                         help='Starting year for events (1904 to 2015).')
-    parser.add_argument('--smonth', dest='start_month', type=int, required=True,
-                        help='Starting month for events (1 to 12).')
+
+    parser.add_argument('--smonth', dest='start_month', required=True,
+                        type=int, help='Starting month for events (1 to 12).')
+
     parser.add_argument('--sday', dest='start_day', type=int, required=True,
                         help='Starting day for events (1 to 31).')
+
     parser.add_argument('--stime', dest='start_time', required=True,
-                        help='Starting time for events HH:MM:SS (00:00:00 to 23:59:59).')
+                        help='''Starting time for events HH:MM:SS
+                        (00:00:00 to 23:59:59).''')
 
     parser.add_argument('--eyear', dest='end_year', type=int, required=True,
                         help='Ending year for events (1904 to 2015).')
+
     parser.add_argument('--emonth', dest='end_month', type=int, required=True,
                         help='Ending month for events (1 to 12).')
+
     parser.add_argument('--eday', dest='end_day', type=int, required=True,
                         help='Ending day for events (1 to 31).')
+
     parser.add_argument('--etime', dest='end_time', required=True,
-                        help='Ending time for events HH:MM:SS (00:00:00 to 23:59:59).')
+                        help='''Ending time for events HH:MM:SS
+                        (00:00:00 to 23:59:59).''')
 
-    ### Depth limits ###
-    parser.add_argument('--Zmin', dest='min_dep', default=argparse.SUPPRESS, type=float,
-                        help='Minimum depth of events (km).')
-    parser.add_argument('--Zmax', dest='max_dep', default=argparse.SUPPRESS, type=float,
-                        help='Maximum depth of events (km).')
+    # --- Depth limits ---
+    parser.add_argument('--Zmin', dest='min_dep', default=argparse.SUPPRESS,
+                        type=float, help='Minimum depth of events (km).')
 
-    ### Magnitude limits ###
-    parser.add_argument('--Mmin', dest='min_mag', default=argparse.SUPPRESS, type=float,
-                        help='Minimum magnitude of events.')
-    parser.add_argument('--Mmax', dest='max_mag', default=argparse.SUPPRESS, type=float,
-                        help='Maximum magnitude of events.')
+    parser.add_argument('--Zmax', dest='max_dep', default=argparse.SUPPRESS,
+                        type=float, help='Maximum depth of events (km).')
 
-    parser.add_argument('--Mtype', dest='req_mag_type', default=argparse.SUPPRESS,
-                        choices=['Any','MB','MS','MW','ML','MD'],
+    # --- Magnitude limits ---
+    parser.add_argument('--Mmin', dest='min_mag', default=argparse.SUPPRESS,
+                        type=float, help='Minimum magnitude of events.')
+
+    parser.add_argument('--Mmax', dest='max_mag', default=argparse.SUPPRESS,
+                        type=float, help='Maximum magnitude of events.')
+
+    parser.add_argument('--Mtype', dest='req_mag_type',
+                        default=argparse.SUPPRESS,
+                        choices=['Any', 'MB', 'MS', 'MW', 'ML', 'MD'],
                         help='''Specific magnitude types. The selected magnitude
-    type will search for all possible magnitudes in that category (e.g. MB will
-    search for mb, mB, Mb, mb1mx, etc).''')
+                                type will search for all possible magnitudes in
+                                that category (e.g. MB will search for mb, mB,
+                                Mb, mb1mx, etc).''')
 
-    parser.add_argument('--Magcy', dest='req_mag_agcy', default=argparse.SUPPRESS,
-                        help='''Limit events to magnitudes computed by the selected
-                        agency: {Any, prime, CODE (specific agency code)}.''')
+    parser.add_argument('--Magcy', dest='req_mag_agcy',
+                        default=argparse.SUPPRESS,
+                        help='''Limit events to magnitudes computed by the
+                                selected agency: {Any, prime, CODE (specific
+                                agency code)}.''')
 
-    ### Defining phases limits ###
-    parser.add_argument('--DPmin', dest='min_def', default=argparse.SUPPRESS, type=int,
-                        help='Minimum number of defining phases.')
-    parser.add_argument('--DPmax', dest='max_def', default=argparse.SUPPRESS, type=int,
-                        help='Maximum number of defining phases.')
+    # --- Defining phases limits ---
+    parser.add_argument('--DPmin', dest='min_def', default=argparse.SUPPRESS,
+                        type=int, help='Minimum number of defining phases.')
 
+    parser.add_argument('--DPmax', dest='max_def', default=argparse.SUPPRESS,
+                        type=int, help='Maximum number of defining phases.')
 
     args = parser.parse_args()
     parser.verify_search_options()
